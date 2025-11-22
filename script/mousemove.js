21a6f7e2d49dd31b42fb1dded291a8da0e4a3ac7
etar9605 <137507028+etar9605@users.noreply.github.com>
2025-11-22 18:57:47 +0000

    d

--- script/mousemove.js
+++ script/mousemove.js
@@ -1,6 +1,16 @@
 document.addEventListener('mousemove', function(event) {
-    var magicCircle = document.getElementById('magic-circle');
+    var magicCursor = document.getElementById('magic-cursor');
     // Căn chỉnh vị trí của con trỏ
-    magicCircle.style.left = event.pageX + 'px';
-    magicCircle.style.top = event.pageY + 'px';
+    magicCursor.style.left = event.pageX + 'px';
+    magicCursor.style.top = event.pageY + 'px';
+});
+
+const cursor = document.querySelector('.magic-cursor');
+
+document.addEventListener('mousedown', () => {
+    cursor.classList.add('active');
+});
+
+document.addEventListener('mouseup', () => {
+    cursor.classList.remove('active');
 });
